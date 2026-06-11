8268f07a562a6627ca6413dbbce0086f65a61237
Trịnh Quốc An <126419371+AnKoCare@users.noreply.github.com>
2026-06-11 15:39:14 +0700

    + fix name

--- Packages/EventTracking/Runtime/Scripts/IAPEventManager.cs
+++ Packages/EventTracking/Runtime/Scripts/IAPEventManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public delegate string GetPriceEvent(string productID);
 public delegate void OnPurchaseEvent(string productID, Action onSuccess, Action onFailure);
-public static class IAPEvent
+public static class IAPEventManager
 {
     public static Action RestorePurchases { get; set; }
     public static GetPriceEvent GetPrice { get; set; }
